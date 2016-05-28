296611c8b3a23466db60ce2c7841aca09c41a34c
Aris Winardi <me@arwin.my.id>
2016-05-29 02:52:40 +0700

    add send mail reset password

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'me@arwin.my.id', 'name' => "Uangku"],
 
     /*
     |--------------------------------------------------------------------------
