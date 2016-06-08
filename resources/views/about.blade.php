9e003b66b8d7d777ee3a77f509c27374f0efc699
Aris Winardi <me@arwin.my.id>
2016-06-09 00:52:02 +0700

    add about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Tentang aplikasi</div>
+
+                <div class="panel-body">
+                    <p>
+                      <h3>UangKu</h3> </br> 
+                      Version 0.001</br>
+                      source <a href="https://github.com/winardiaris/uangku-laravel" target="_blank">Github</a> </br> 
+                      license <a href="https://creativecommons.org/licenses/by-sa/3.0/" target="_blank">cc by-sa 3.0</a>
+                      </br>
+                      </br>
+                      </br>
+                      </br>
+                      <i>winardiaris</i>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
