9c7b88c9a31e4a37de62c4620ae0fe398bb97980
Aris Winardi <me@arwin.my.id>
2016-06-09 02:58:28 +0700

    solved date with null value in type in/out

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,8 @@
                   @include('data.search',['url'=>'home','link'=>'home'])
                   <div id="chart">
                     <canvas id="myChart" width="1100" height="600"></canvas>
+                    <hr>
+                    <i>Banyak data: <span id="banyak"></span></i>
                   </div>
                 </div>
             </div>
@@ -24,16 +26,42 @@ var json = <?php echo json_encode($data); ?>;
 var labels = [];
 var dataPemasukan = [];
 var dataPengeluaran = [];
+var ins=[];
+var outs=[];
 for(i=0;i<json.length;i++){
   labels.push(json[i].date);
   if(json[i].type=='in'){
-    dataPemasukan.push(json[i].value);
+   ins.push(json[i].date) ;
   }else{
-    dataPengeluaran.push(json[i].value);
+    outs.push(json[i].date) ;
   }
 }
-var labelChart = jQuery.unique(labels);
 
+var labelChart = jQuery.unique(labels);
+for(j=0;j<json.length;j++){
+
+  var adains = jQuery.inArray(json[j].date,outs);
+  var adaouts = jQuery.inArray(json[j].date,ins);
+
+  /* console.log(json[j].date+":"+adains+":"+adaouts); */
+  if(adains>=0 && adaouts>=0){
+    if(json[j].type=='in'){
+      dataPemasukan.push(json[j].value);
+    }else{
+      dataPengeluaran.push(json[j].value);
+    }
+  }
+  else if(adains>=0 && adaouts<0){
+      dataPemasukan.push(0);
+      dataPengeluaran.push(json[j].value);
+  }
+  else if(adains<0 && adaouts>=0){
+      dataPemasukan.push(json[j].value);
+      dataPengeluaran.push(0);
+  }
+}
+/* console.log('in: '+ dataPemasukan); */
+/* console.log('out: '+ dataPengeluaran); */
 var config =  {
               type:'line',
               data:{
@@ -71,9 +99,9 @@ var config =  {
     
               };
 
-  var ctx = document.getElementById("myChart").getContext("2d");
-  /* window.myLine = new Chart(ctx, config); */
-  var myLineChart = Chart.Line(ctx,config);
-//BELUM jika ada data kosong pada tanggal harusnya pada tanggal tersebut value=0                      
+var ctx = document.getElementById("myChart").getContext("2d");
+var myLineChart = Chart.Line(ctx,config);
+
+$('#banyak').text(json.length);
 </script>
 @endsection
