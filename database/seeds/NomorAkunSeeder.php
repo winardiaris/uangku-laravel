4a45417f916aa71b959a478eccb024284b43d858
Aris Winardi <ariswinardy@gmail.com>
2017-09-09 16:30:12 +0700

    remove whitespace

--- database/seeds/NomorAkunSeeder.php
+++ database/seeds/NomorAkunSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use League\Csv\Reader; 
+use League\Csv\Reader;
 class NomorAkunSeeder extends Seeder
 {
     /**
@@ -11,16 +11,16 @@ class NomorAkunSeeder extends Seeder
      */
     public function run()
     {
-      $file = public_path().'/datas/data_nomor_akun.csv'; 
+      $file = public_path().'/datas/data_nomor_akun.csv';
       $csv = Reader::createFromPath($file);
       $nbInsert = $csv->each(function ($row){
               if(empty($row)) return false;
-                       
+
               return  \DB::table('nomor_akun')->insert(
                 array(
                   'nomor_akun' => $row[0],
-                  'sub' => $row[1], 
-                  'nama_akun' => $row[2], 
+                  'sub' => $row[1],
+                  'nama_akun' => $row[2],
                 )
               );
       });
